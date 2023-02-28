4c7da69cdea7d60d5575345b4a0b12eab1ee6bb3
marion brothier <arnaudvv@hotmail.fr>
2023-02-28 17:53:45 +0100

    mise en place des img

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-  $('.carrousel-container').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
+$('.slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
