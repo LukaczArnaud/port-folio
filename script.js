a92c61d982371c5416dcb44d8e904294c2cb9fae
marion brothier <arnaudvv@hotmail.fr>
2023-03-01 14:47:40 +0100

    media query mobile

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-$('.slider').slick({
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  autoplay: false,
-  autoplaySpeed: 3000,
-});
+if (window.matchMedia("(min-width: 599px)").matches) {
+  $('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: false,
+    autoplaySpeed: 3000,
+  });
+} else {
+  $('.slider').slick();
+
+  }
