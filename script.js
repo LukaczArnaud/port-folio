6313536875e886b201b22b8fc76eca9e66bdd324
marion brothier <arnaudvv@hotmail.fr>
2023-03-07 11:35:58 +0100

    maj .js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 if (window.matchMedia("(min-width: 599px)").matches) {
   $('.slider').slick({
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+} else {
+  $('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
   });
-} else {
-  $('.slider').slick();
   }
